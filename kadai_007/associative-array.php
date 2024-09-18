62132b15704784ea085fe11c235c4f97f478b43b
sukinanoha15 <isaoyuua1201@icloud.com>
2024-09-18 23:42:10 +0900

    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>007課題</title>
+    </head>
+    <body>
+        <p>
+            <?php
+            $data=['name'=>'onion', 'price'=>200, 'weight'=>160];
+            print_r($data);
+            ?>
+        </p>
+    </body>
+</html>
