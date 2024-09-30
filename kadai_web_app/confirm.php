d4cc224d50f114379711c9349fe6d9b8907cc447
sukinanoha15 <isaoyuua1201@icloud.com>
2024-09-30 20:30:19 +0900

    kadai_web_app

---
+++ kadai_web_app/confirm.php
@@ -0,0 +1,38 @@
+<?php
+$name = $_POST['employee_name'];
+$age = $_POST['employee_age'];
+$department = $_POST['department'];
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th>項目</th>
+            <th>入力内容</th>
+        </tr>
+        <tr>
+            <td>社員名</td>
+            <td><?php echo $name; ?></td>
+        </tr>
+        <tr>
+            <td>年齢</td>
+            <td><?php echo $age; ?></td>
+        </tr>
+        <tr>
+            <td>所属部署</td>
+            <td><?php echo $department; ?></td>
+        </tr>
+    </table>
+    <p>
+        <button id="confirm-btn" onclick="location.href='complete.php';">確定</button>
+        <button id="cancel-btn" onclick="history.back();">キャンセル</button>
+    </p>
+</body>
+</html>
